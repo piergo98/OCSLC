172d92dc42a386e2f0e14d8f5fb63d3e428160ff
piergo98 <p.gori3@studenti.unipi.it>
2024-10-23 18:58:55 +0200

    example for debugging the algorithm

--- examples/example_non_autonomous_linear.py
+++ examples/example_non_autonomous_linear.py
@@ -1,6 +1,7 @@
 import time
 
 import casadi as ca
+from matplotlib import pyplot as plt
 import numpy as np
 
 from ocslc.switched_linear_mpc import SwitchedLinearMPC
@@ -18,7 +19,7 @@ def test_non_autonomous_switched_linear_3():
     n_phases = 40
     time_horizon = 1
     
-    Q = 100. * np.eye(n_states)
+    Q = 1000. * np.eye(n_states)
     R = 1. * np.eye(n_inputs)
     E = 0. * np.eye(n_states)
 
@@ -26,7 +27,7 @@ def test_non_autonomous_switched_linear_3():
     
     xr = np.array([1, 2])
 
-    swi_lin_mpc = SwitchedLinearMPC(model, n_phases, time_horizon, auto=False, multiple_shooting=True, x0=x0)
+    swi_lin_mpc = SwitchedLinearMPC(model, n_phases, time_horizon, auto=False, multiple_shooting=False, x0=x0)
 
     swi_lin_mpc.precompute_matrices(x0, Q, R, E)
     
@@ -46,15 +47,26 @@ def test_non_autonomous_switched_linear_3():
     # swi_lin_mpc.add_constraint(final_state_constr, final_state_lb, final_state_ub)
 
     swi_lin_mpc.set_cost_function(Q, R, x0)
-    
-    # Set the initial guess  
-    swi_lin_mpc.set_initial_guess(time_horizon, x0)
 
-    swi_lin_mpc.create_solver()
+    optimal_cost = []
+    for iter_num in range(1, 227):
+        # Set the initial guess  
+        swi_lin_mpc.set_initial_guess(time_horizon, x0)
+        swi_lin_mpc.create_solver(max_iter=iter_num)
 
-    inputs_opt, deltas_opt, _ = swi_lin_mpc.solve()
+        # DSS: 226 iterations
+        # DMS: 1960 iterations
+        inputs_opt, deltas_opt, _ = swi_lin_mpc.solve()
+        optimal_cost.append(swi_lin_mpc.opt_cost)
     
-    swi_lin_mpc.plot_optimal_solution(deltas_opt, inputs_opt)
+    x_axes = np.arange(1, 227, 1)
+    plt.plot(x_axes, optimal_cost)
+    plt.grid()
+    plt.xlabel('Iterations')
+    plt.ylabel('Optimal Cost')
+    
+    plt.show()
+    # swi_lin_mpc.plot_optimal_solution(deltas_opt, inputs_opt)
     
 if __name__ == '__main__':
     start = time.time()
